4ec4aea63a54ebe54ac196d2babb168afe8d2e9d
yacaeh <yacaeh@naver.com>
2019-03-25 00:43:03 +0000

    update server.sh file to restart program on crash

--- server/server.sh
+++ server/server.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 export PYTHONPATH='/workspace:/workspace/server' 
-python server.py
+export LANG=ko_KR.UTF-8
+while ! python server.py
+do
+   sleep 1
+   echo "Restarting Server"
+done
